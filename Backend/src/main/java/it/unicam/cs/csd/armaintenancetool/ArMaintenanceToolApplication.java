f7e02aa4abc2db644c66e616e7239e961a0391a2
Vlad Dogariu <vlad.dogariu@studenti.unicam.it>
2022-11-23 16:22:34 +0100

    Generating Spring Project for Backend
    Init Backend

---
+++ Backend/src/main/java/it/unicam/cs/csd/armaintenancetool/ArMaintenanceToolApplication.java
@@ -0,0 +1,13 @@
+package it.unicam.cs.csd.armaintenancetool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArMaintenanceToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArMaintenanceToolApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/it/unicam/cs/csd/armaintenancetool/ArMaintenanceToolApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unicam.cs.csd.armaintenancetool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArMaintenanceToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
